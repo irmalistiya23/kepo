d43d7e3b66576f1b888225bde1a9809ee8e44376
Hubbi502 <hubbielfairuz8@gmail.com>
2025-04-15 20:51:17 +0700

    Update dependencies, add README and Vercel configuration, and refine import statements

--- backend/src/controllers/goals.controller.js
+++ backend/src/controllers/goals.controller.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 import { Decimal } from "@prisma/client/runtime/library";
 
 const prisma = new PrismaClient();
